e2f6e01fa12f1724905862065b8d57397df6641b
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-07-21 10:07:17 +0200

    👌 IMPROVE:  fetch et serveur ok, créa fonction pour injecter produits sur page accueil

---
+++ front/html/mastermind.js
@@ -0,0 +1,15 @@
+
+
+//Etape 1
+
+fetch( 'http://localhost:3000/api/products') .then((res) => {return res.json()}) .then((data)=>console.log(data));
+
+
+ for (let _id of 'name') {console.log(_id)};
+document.getElementById('items');
+items.append('produits');
+
+
+
+
+
