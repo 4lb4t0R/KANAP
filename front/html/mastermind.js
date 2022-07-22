6fa4bb5704667a56c3abcd7d7281ae774afb0622
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-07-22 08:30:38 +0200

    tentative cré ajout

--- front/html/mastermind.js
+++ front/html/mastermind.js
@@ -5,11 +5,24 @@
 fetch( 'http://localhost:3000/api/products') .then((res) => {return res.json()}) .then((data)=>console.log(data));
 
 
- for (let _id of 'name') {console.log(_id)};
+for (let _id of 'name') {console.log(_id)};
 
 document.getElementById('items');
 
 items.prepend('_id');
+
+let listeKanap = [];
+
+document.getElementById('items').innerHTML= listeKanap.map((kanapes)=>
+<div id="produit$(kanapes._id)" class= "card, card-size">
+<h3 class="titre-card">$(kanapes.name.toUpperCase)
+</h3>
+<img class="image-size" src="$(kanapes.imageUrl)" alt="image du meuble $(kanapes.name)"/>
+ <p class="description-meuble"> $(kanapes.description)</p>
+ <button id="$(kanapes.id)" class="bouton-details"></button>
+ <p>$(kanapes.price.toString().replace(/0+$/,"")) Euro</p>
+ </div>
+ )
 
 
 
