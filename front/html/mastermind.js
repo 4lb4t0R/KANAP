5ca757fc34e4e55a6c5bda306a7d451252b5eee4
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-07-25 11:58:57 +0200

    prblm syntaxe, doit voir avec antho sinon ça marche jpense

--- front/html/mastermind.js
+++ front/html/mastermind.js
@@ -11,21 +11,25 @@ document.getElementById('items');
 
 items.prepend('_id');
 
-function ("affichageProduits") {
-    let url = 'http://localhost:3000/api/products';
-    fetch(url).then ((response) => response.json().then((data) => {console.log(data);
-    let affichage ='<div>';
+
+
+let url = 'http://localhost:3000/api/products'
+
+function affichageProduits ({
+    fetch(url).then((res) => {return res.json()}).then(data => console.log(data));
+    let affichage ='<div>' ,
     for (let produit of data) {
     affichage += 
     '<h3 class="titre-card">$(produit.nom)</h3>'
-     '<img class="image-size" src="$(produit.imageUrl)" alt="image du meuble $(produit.name)"/>'
+    '<img class="image-size" src="$(produit.imageUrl)" alt="image du meuble $(produit.name)"/>'
     '<p class="description-meuble"> $(produit.description)</p>'
     '<button id="$(produit.id)" class="bouton-details"></button>'
     '<p>$(produit.price.toString().replace(/0+$/,""))Euro</p>';}
     affichage += '</div>';
-    document.querySelector('#items').innerHTML= affichage;})
-    );
-    };
+    document.querySelector('#items').innerHTML= affichage;
+});
+    
+    
 
 
 
