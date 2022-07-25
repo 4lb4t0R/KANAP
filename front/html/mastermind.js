a6a3769247eca07ac702ce2be72b83655b943f85
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-07-25 11:09:30 +0200

    👌 IMPROVE:  commit depuis pc fixe

--- front/html/mastermind.js
+++ front/html/mastermind.js
@@ -11,18 +11,21 @@ document.getElementById('items');
 
 items.prepend('_id');
 
-let listeKanap = [];
-
-document.getElementById('items').innerHTML= listeKanap.map((kanapes)=>
-`<div id="produit$(kanapes._id)" class= "card, card-size">
-<h3 class="titre-card">$(kanapes.name.toUpperCase)
-</h3>
-<img class="image-size" src="$(kanapes.imageUrl)" alt="image du meuble $(kanapes.name)"/>
- <p class="description-meuble"> $(kanapes.description)</p>
- <button id="$(kanapes.id)" class="bouton-details"></button>
- <p>$(kanapes.price.toString().replace(/0+$/,"")) Euro</p>
- </div>`
- );
+function () {
+    let url = 'http://localhost:3000/api/products';
+    fetch(url).then ((response) => response.json().then((data) => {console.log(data);
+    let affichage ='<div>';
+    for (let produit of data) {
+    affichage += 
+    '<h3 class="titre-card">$(produit.nom)</h3>'
+     '<img class="image-size" src="$(produit.imageUrl)" alt="image du meuble $(produit.name)"/>'
+    '<p class="description-meuble"> $(produit.description)</p>'
+    '<button id="$(produit.id)" class="bouton-details"></button>'
+    '<p>$(produit.price.toString().replace(/0+$/,""))Euro</p>';}
+    affichage += '</div>';
+    document.querySelector('#items').innerHTML= affichage;})
+    );
+    };
 
 
 
