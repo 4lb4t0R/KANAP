0f57861f934e1bd0b4c2172da031281d62633ff4
4lb4t0R <h4p4st4n7ir@gmail.com>
2022-08-31 14:49:03 +0200

    👌 IMPROVE:  script pague d'accueil corrigé et full commenté

--- front/js/mastermind.js
+++ front/js/mastermind.js
@@ -1,29 +1,30 @@
 
-
+/*
+ *  récupération des données sur produits vendus, depuis l'API de l'entreprise
+*/
 
 fetch("http://localhost:3000/api/products")
- 
   .then((res) => res.json())
-  
   .then((objetProduits) => {
-    
     console.table(objetProduits);
-    
     affichageProduits(objetProduits);
   })
-  
   .catch((err) => {
     document.querySelector(".titles").innerHTML = "<h1>erreur 404</h1>";
     console.log("erreur 404, sur ressource api:" + err);
   });
 
+
+/*
+* fonction affichant les produits via des balises insérées dans le HTML
+*/
+
 function affichageProduits(index) {
-  
   let zoneArticle = document.querySelector("#items");
- 
+  let html = "";
+
   for (let article of index) {
-    
-    zoneArticle.innerHTML += `<a href="./product.html?_id=${article._id}">
+    html += `<a href="./product.html?_id=${article._id}">
     <article>
       <img src="${article.imageUrl}" alt="${article.altTxt}">
       <h3 class="productName">${article.name}</h3>
@@ -31,6 +32,8 @@ function affichageProduits(index) {
     </article>
   </a>`;
   }
+
+  zoneArticle.innerHTML = html;
 }
     
 
